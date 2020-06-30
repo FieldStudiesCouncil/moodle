b4c25d3f4162abdffe131246872d65c035d202c0
FSC Moodle Admin <moodle.admin@field-studies-council.org>
2020-06-30 01:29:24 +0000

    Update Safe Exam Browser access rules plugin

--- mod/quiz/accessrule/seb/settings.php
+++ mod/quiz/accessrule/seb/settings.php
@@ -35,6 +35,11 @@ if ($hassiteconfig) {
         '',
         $OUTPUT->notification(get_string('setting:supportedversions', 'quizaccess_seb'), 'warning')));
 
+    $settings->add(new admin_setting_heading('quizaccess_seb/templates',
+        '',
+        html_writer::link(new moodle_url('/mod/quiz/accessrule/seb/template.php'), get_string('manage_templates', 'quizaccess_seb'))
+    ));
+
     $settings->add(new admin_setting_configcheckbox('quizaccess_seb/autoreconfigureseb',
         get_string('setting:autoreconfigureseb', 'quizaccess_seb'),
         get_string('setting:autoreconfigureseb_desc', 'quizaccess_seb'),
